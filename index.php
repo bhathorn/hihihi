b7ead2d2e9b212260c85095af1c8d9c05228db19
bhathorn <73677715+bhathorn@users.noreply.github.com>
2020-10-30 10:46:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello it<br>";
-?>
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+        <body>
+        <h1>ภาธร วัฒน์ประกายรัตน์ แทน</h1>
+        <p>63070133</p>
+        <img src="skyme.jpg">
+        <img src="chinchin.jpg">
+        <br>
+        <a href="https://youtu.be/ztO3Rjqxcho">this is a link</a>
+
+        <h2>ตลก3[iimyf</h2>
+        <p>ลูก: วันนี้ที่โรงเรียนสุดยอดมากเลยแม่ พวกเราทำระเบิดกันด้วย</p>
+        <p>แม่: ว้าวๆ ทำอะไรสุดโต่งไปเลยนะ โรงเรียนของลูกเนี่ย แล้วพรุ่งนี้ลูกจะทำอะไรที่โรงเรียนเหรอ</p>
+        <p>ลูก: โรงเรียน? ยังมีโรงเรียนอยู่เหรอแม่?</p>
+   </body>
+</html>
